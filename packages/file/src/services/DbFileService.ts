998fea61d818a5dfd491b2f0de296e98e3fc1bfd
Brent Bahry <brentbahry@gmail.com>
2024-06-24 23:47:55 -0700

    updated `DbFile` to expect file data to be a string

--- packages/file/src/services/DbFileService.ts
+++ packages/file/src/services/DbFileService.ts
@@ -5,10 +5,10 @@ import { File } from '../tables/FileTable';
 export const getDbFileService = serviceFactory<DbFileService>('@proteinjs/db-file/DbFileService');
 
 export interface DbFileService extends Service {
-  createFile(fileMetaData: Omit<File, keyof ScopedRecord>, fileData: string[]): Promise<File>;
+  createFile(fileMetaData: Omit<File, keyof ScopedRecord>, fileData: string): Promise<File>;
   getFile(fileId: string): Promise<File>;
-  getFileData(fileId: string): Promise<string[]>;
-  updateFileData(fileId: string, data: string[]): Promise<void>;
+  getFileData(fileId: string): Promise<string>;
+  updateFileData(fileId: string, data: string): Promise<void>;
   updateFile(file: Omit<File, keyof ScopedRecord>): Promise<void>;
   deleteFile(fileId: string): Promise<void>;
 }
