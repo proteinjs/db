0b2bbf91c8bde36133ab54c1ab6c5fdae0096a94
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2024-06-27 15:21:51 -0700

    create new query builder instance in table loader

--- packages/ui/src/table/QueryTableLoader.ts
+++ packages/ui/src/table/QueryTableLoader.ts
@@ -1,33 +1,50 @@
-import { Query, QueryBuilderFactory, Record, SortCriteria, Table, getDb } from '@proteinjs/db';
+import { Query, QueryBuilder, QueryBuilderFactory, Record, SortCriteria, Table, getDb } from '@proteinjs/db';
 import { RowWindow, TableLoader } from '@proteinjs/ui';
-import { Debouncer, Logger } from '@proteinjs/util';
+import { Debouncer } from '@proteinjs/util';
 
 export class QueryTableLoader<T extends Record> implements TableLoader<T> {
-  private logger = new Logger('QueryTableLoader', 'info', 10000);
+  private rowCountQb?: QueryBuilder<T>;
+  private paginationQb?: QueryBuilder<T>;
   constructor(
     private table: Table<T>,
     private query?: Query<T>,
     private sort?: SortCriteria<T>[],
     private debouncer?: Debouncer
-  ) {}
+  ) {
+    // Store separate copies of the query for row count and pagination
+    this.rowCountQb = new QueryBuilderFactory().createQueryBuilder(this.table, this.query);
+    this.paginationQb = new QueryBuilderFactory().createQueryBuilder(this.table, this.query);
+  }
 
   async load(startIndex: number, endIndex: number): Promise<RowWindow<T>> {
-    const db = getDb();
-    const sort: any = this.sort ? this.sort : [{ field: 'created', desc: true }];
-    // veronica todo: bunching these into a single await possibly created some issues with the query getting built
-    // the rowCount query was getting some things from the rows query (pagination and sorting) which it shouldn't have
-    // doing it one at a time is technically a performance hit
-    const rowcountQb = new QueryBuilderFactory().getQueryBuilder(this.table, this.query);
-    // const rowCountPromise = db.getRowCount(this.table, rowcountQb);
-    const totalCount = await db.getRowCount(this.table, rowcountQb);
+    const executeQuery = async (): Promise<{
+      rows: T[];
+      totalCount: number;
+    }> => {
+      const db = getDb();
+      const sort: any = this.sort ? this.sort : [{ field: 'created', desc: true }];
 
-    const qb = new QueryBuilderFactory()
-      .getQueryBuilder(this.table, this.query)
-      .sort(sort)
-      .paginate({ start: startIndex, end: endIndex });
-    const rows = await db.query(this.table, qb);
-    // const [rows, totalCount] = await Promise.all([queryPromise, rowCountPromise]);
-    this.logger.info(`rowCount: ${totalCount}`);
-    return { rows, totalCount };
+      const rowCountPromise = db.getRowCount(this.table, this.rowCountQb);
+      const qb = new QueryBuilderFactory()
+        .createQueryBuilder(this.table, this.paginationQb)
+        .sort(sort)
+        .paginate({ start: startIndex, end: endIndex });
+      const queryPromise = db.query(this.table, qb);
+
+      const [rows, totalCount] = await Promise.all([queryPromise, rowCountPromise]);
+      return { rows, totalCount };
+    };
+
+    if (this.debouncer) {
+      const debouncer = this.debouncer;
+      return new Promise<RowWindow<T>>((resolve) => {
+        debouncer.debounce(async () => {
+          const result = await executeQuery();
+          resolve(result);
+        });
+      });
+    } else {
+      return await executeQuery();
+    }
   }
 }
