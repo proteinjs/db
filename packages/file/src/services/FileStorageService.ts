8e497db7c2e8fc3e5df068ae04f13e460d13a005
Brent Bahry <brentbahry@gmail.com>
2024-06-26 17:44:32 -0700

    renamed `DbFile` -> `FileStorage` since we were getting naming conflicts on the client. this is probably more accurate anyway.

--- packages/file/src/services/FileStorageService.ts
+++ packages/file/src/services/FileStorageService.ts
@@ -2,9 +2,9 @@ import { Service, serviceFactory } from '@proteinjs/service';
 import { ScopedRecord } from '@proteinjs/user';
 import { File } from '../tables/FileTable';
 
-export const getDbFileService = serviceFactory<DbFileService>('@proteinjs/db-file/DbFileService');
+export const getFileStorageService = serviceFactory<FileStorageService>('@proteinjs/db-file/FileStorageService');
 
-export interface DbFileService extends Service {
+export interface FileStorageService extends Service {
   createFile(fileMetaData: Omit<File, keyof ScopedRecord>, fileData: string): Promise<File>;
   getFile(fileId: string): Promise<File>;
   getFileData(fileId: string): Promise<string>;
